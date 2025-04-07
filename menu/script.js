89cc44d20465ec7b2fab3a6fb18c3be15c087a94
JV <jvking130@gmail.com>
2025-04-07 09:22:13 -0300

    third commit

--- menu/script.js
+++ menu/script.js
@@ -2,11 +2,12 @@ let telaPreta = document.getElementById("telaPreta");
 let jumpscareVideo = document.getElementById("jumpscareVideo");
 let menu = document.getElementById("menu");
 let tempoParaParar = 25.083; 
+let iniciarButton = document.getElementsByClassName("glitch-text-iniciar")
 
 document.addEventListener("keydown", function iniciar() {
     document.removeEventListener("keydown", iniciar); 
     
-    jumpscareVideo.currentTime = 25.083; 
+    jumpscareVideo.currentTime = 19;  //test, 19s
     
     setTimeout(() => {
         telaPreta.style.display = "none";
@@ -26,7 +27,21 @@ jumpscareVideo.addEventListener("timeupdate", function verificarTempo() {
 jumpscareVideo.addEventListener("ended", function() {
     jumpscareVideo.style.display = "none";
     menu.style.display = "flex";
-    menu.style.backgroundImage = 'url("/menu.png")'; 
+    menu.style.backgroundImage = 'url("/menu/menu.png")'; 
     menu.style.backgroundSize = "cover"; 
-    menu.style.backgroundPosition = "center"; 
 });
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    const glitchTexts = document.querySelectorAll(".glitch-text-iniciar, .glitch-text-opcoes, .glitch-text-creditos, .glitch-text-sair");
+
+    glitchTexts.forEach(el => {
+     
+      const delay = (Math.random() * 5).toFixed(2) + 's';
+      el.style.animationDelay = delay;
+    });
+  });
+
+  function startGame(){
+        window.location.href = "/game/game.html"
+  }
